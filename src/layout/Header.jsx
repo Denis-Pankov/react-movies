7669eb7ddd0ccc5b99aa9ded7622a71d7b669dbe
Denis-Pankov <angelman777@mail.ru>
2023-01-02 04:10:30 +0300

    .

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <nav className="purple lighten-3">
       <div className="nav-wrapper">
-        <a href="!#" className="brand-logo">
+        <a href="/" className="brand-logo">
           React Movies
         </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
