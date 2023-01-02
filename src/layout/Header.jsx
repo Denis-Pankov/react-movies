092bbd4147acc9d289893f47bdb13b3ed8a70ba7
Denis-Pankov <angelman777@mail.ru>
2023-01-02 04:04:45 +0300

    add repo

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
         </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li>
-            <a href="!#">Repo</a>
+            <a href="https://github.com/Denis-Pankov/react-movies">Repo</a>
           </li>
         </ul>
       </div>
